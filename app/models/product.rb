e959d1da218e1603085eb3532d5a4b9be8e693ef
Edward Deam <edwardcrdeam@gmail.com>
2020-05-03 19:26:49 -0700

    Product model challenge

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, length: { in: 5..29 }
+  validates :category, presence: true, length: { minimum: 5, maximum: 24}
   validates :price, presence: true, numericality: { greater_than: 0 }
+
+  def display_currency
+    '$' + sprintf('%.2f', (self.price / 100.0))
+  end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,23 +2,56 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   subject { Product.new(name: 'Test Product', category: 'Widget', price: 500) }
-  
-  it 'is valid with valid attributes' do
-    expect(subject).to be_valid
+
+  context 'validations' do
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid without a name' do
+      subject.name = nil
+      expect(subject).to_not be_valid 
+    end
+
+    it 'is not valid with a name less than five characters' do
+      subject.name = 'abcd'
+      expect(subject).to_not be_valid 
+    end
+
+    it 'is not valid with a name greater than twenty-nine characters' do
+      subject.name = 'a'*30
+      expect(subject).to_not be_valid 
+    end
+
+    it 'is not valid without a price' do
+      subject.price = nil
+      expect(subject).to_not be_valid 
+    end
+
+    it 'is not valid with a price less than one cent' do
+      subject.price = 0
+      expect(subject).to_not be_valid 
+    end
+
+    it 'is not valid without a category' do
+      subject.category = nil
+      expect(subject).to_not be_valid 
+    end
+
+    it 'is not valid with a name less than five characters' do
+      subject.category = 'abcd'
+      expect(subject).to_not be_valid 
+    end
+
+    it 'is not valid with a name greater than twenty-five characters' do
+      subject.category = 'a'*25
+      expect(subject).to_not be_valid 
+    end
   end
 
-  it 'is not valid without a name' do
-    subject.name = nil
-    expect(subject).to_not be_valid 
-  end
-
-  it 'is not valid without a price' do
-    subject.price = nil
-    expect(subject).to_not be_valid 
-  end
-
-  it 'is not valid with a price less than one cent' do
-    subject.price = 0
-    expect(subject).to_not be_valid 
+  context '#display_currency' do
+    it 'converts the price to the string format' do
+      expect(subject.display_currency).to eql('$5.00')
+    end
   end
 end
